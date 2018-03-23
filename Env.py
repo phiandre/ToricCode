5fa4ac4c76d7caed4354d44da9447de656e307b4
Joel Johansson <gusjohjodt@student.gu.se>
2018-03-23 15:33:32 +0100

    Kommenterade koden och fixade problemet med att det skapas massor av ettor, finns en del kvar att göra tho

--- Env.py
+++ Env.py
@@ -26,54 +26,64 @@ class Env:
 		temp = np.zeros([self.length * self.length, 2], dtype=np.int8)
 		x = 0
 
-		for i in range(self.length):
+		for i in range(self.length):		#Kollar igenom tillståndet och sparar felen i en array
 			for j in range(self.length):
 				if self.state[i,j] == 1:
 					temp[x,0:2] = [i, j]
 					x += 1
 
-		self.errors = temp[0:x,0:2]
+		self.errors = temp[0:x,0:2]			# Arrayen innehåller positionen för alla fel
+
 
 	"""
 	Flyttar errors, och släcker ut som två errors möter varandra.
 		@param
 	Action: "u - 0" "d - 1" "l - 2" "r - 3"
 	"""
-	def moveError(self, action, errorIndex):
+	def moveError(self, action, errorIndex):   			# Tar in ett fel och vilken action den ska ta
 
-		firstPos = self.errors[errorIndex, :]
-		secondPos = self.getPos(action, firstPos)
+		firstPos = self.errors[errorIndex, :]			# Positionen för felet som skall flyttas
+		secondPos = self.getPos(action, firstPos)		# Nya positionen för felet givet action och position
+		print("2ndPos")
 		print(secondPos)
 		self.state[firstPos] = 0
-		print(self.state)
+		print("secondPos")
 		print(secondPos)
-		self.state[secondPos] = 1 - self.state[secondPos]
+		if self.state[secondPos[0],secondPos[1]] == 0:
+			self.state[secondPos] = 1
+		else:
+			self.state[secondPos] = 0
+
+		print("newstate")
 		print(self.state)
 
-		self.updateErrors()
 
-	def getPos(self, action, position):
+		self.updateErrors()									# Kolla igenom igen vart fel finns
+
+	def getPos(self, action, position):					# Input: vilken action, felets position
 
 		nextPos = np.array(position, copy=True)
+		print("nextpos")
 		print(nextPos)
-		if action == 0:
+		if action == 0:									# Beroende på action väljs steg
 			nextPos[0] -= 1
 			if nextPos[0] == 0:
-				nextPos[0] = self.length - 1
+				nextPos[0] = self.length			#Flyttar genom väggen
 		if action == 1:
 			nextPos[0] += 1
-			if nextPos[0] == self.length - 1:
+			if nextPos[0] == self.length:
 				nextPos[0] = 0
 		if action == 2:
 			nextPos[1] -= 1
 			if nextPos[1] == 0:
-				nextPos[1] = self.length - 1
+				nextPos[1] = self.length
 		if action == 3:
 			nextPos[1] += 1
-			if nextPos[1] == self.length - 1:
+			if nextPos[1] == self.length:
 				nextPos[1] = 0
-
-		return nextPos
+		print("newpos")
+		print(nextPos)
+		return nextPos									# Output: nya positionen för felet
 
 if __name__ == '__main__':
 
@@ -82,12 +92,13 @@ if __name__ == '__main__':
 	S[1,1] = 1
 	S[2,1] = 1
 	env = Env(S)
-
+	print("Start state:")
 	print(env.state)
 
 	env.moveError(1,1)
-
+	print("final state")
 	print(env.state)
+
 
 
 
