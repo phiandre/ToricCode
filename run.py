cc5978187b213ddd92d0bec7977effb99cfff2ec
SimonDaNinja <37663175+SimonDaNinja@users.noreply.github.com>
2018-03-27 11:45:01 +0200

    Runfilen; än så länge enbart centrering påbörjad

---
+++ run.py
@@ -0,0 +1,19 @@
+import numpy as np
+from Env import Env
+from GenerateToricData import Generate
+
+
+
+humRep,comRep = Generate.generateData(3,1)
+E=Env(comRep)
+
+print(humRep)
+print(comRep)
+
+
+errors=E.getErrors()
+
+
+def centralize
+	for x in range(n):
+	
