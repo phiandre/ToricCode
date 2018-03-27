554a12e0057bdf417e923bc349b156845c5397d5
nihilsciens <hannouch@student.chalmers.se>
2018-03-27 15:55:56 +0200

    kan simulera en körning

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 import numpy as np
+from RL import RLsys
 from Env import Env
 from GenerateToricData import Generate
 
+
+"""
 size=5
 flips=3
 
@@ -14,4 +17,26 @@ E=Env(comRep)
 
 observation=E.getObservation()
 # observation är den observationstensor som skickas till RL-hjärnan
+"""
 
+
+"""""""""""""""""""""""""""""""""""""""""""""
+Mainmetod, här körs själva simuleringen.
+"""""""""""""""""""""""""""""""""""""""""""""
+if __name__ == '__main__':
+	
+	flip = np.arange(5)
+	size=5
+	actions=4
+
+	rl = RLsys(4, size)
+	
+	humRep, state = Generate.generateData(size, 1)
+	env = Env(state)
+
+	while len(env.getErrors()) > 0:
+		print(state)
+		observation = env.getObservation()
+		a, e = rl.choose_action(observation)
+		r = env.moveError(a, e)
+			
