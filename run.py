bc7d3c2cf5173e695510c155e40be4aa458670d9
SimonDaNinja <37663175+SimonDaNinja@users.noreply.github.com>
2018-03-27 14:07:20 +0200

    Centralize är klar

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from GenerateToricData import Generate
 
 
 
-humRep,comRep = Generate.generateData(3,1)
+humRep,comRep = Generate.generateData(5,3)
 E=Env(comRep)
 
 print(humRep)
@@ -14,6 +14,14 @@ print(comRep)
 errors=E.getErrors()
 
 
-def centralize
-	for x in range(n):
-	
+def centralize(state,error):
+	# state är matrisen som karaktäriserar tillståndet
+	# error är koordinaterna för felet
+	state_=np.concatenate((state[:,error[1]:],state[:,0:error[1]]),1)
+	state_=np.concatenate((state_[error[0]:,:],state_[0:error[0],:]),0)
+	rowmid=int(np.ceil(state.shape[0]/2))
+	colmid=int(np.ceil(state.shape[1]/2))
+	state_=np.concatenate((state_[:,colmid:],state_[:,0:colmid]),1)
+	state_=np.concatenate((state_[rowmid:,:],state_[0:rowmid,:]),0)
+	return state_
+
