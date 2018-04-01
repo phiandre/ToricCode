fc8a68584c06163f62567ff4c5f7c617352d3bbc
niklasforsstrom <32497192+niklasforsstrom@users.noreply.github.com>
2018-04-01 15:58:02 +0200

    sparar filer med rätt namn + variabel för max number of iterations

--- run_train.py
+++ run_train.py
@@ -8,61 +8,70 @@ import pickle
 
 
 class MainClass:
-	
-	
-	def __init__(self):
-		# creates a new filename each time we run the code
-		tmp = list('numSteps1.npy')
-		static_element = 1
-		while os.path.isfile("".join(tmp)):
-			static_element += 1
-			tmp[8] = str(static_element)
-		self.filename="".join(tmp)
-		
-		self.run()
-        
-	def run(self):
-		flip = np.arange(5)
-		size=5
-		actions=4
 
-		rl = RLsys(4, size)
-		
-		humRep=np.load('ToricCodeHuman.npy')
-		comRep=np.load('ToricCodeComputer.npy')
-		print(comRep[:,:,3])
-		
-		iterations = np.zeros(comRep.shape[2])
-		
-		for i in range(comRep.shape[2]):
-			state=comRep[:,:,i]
-			env = Env(state)
-			numIter = 0
-			
-			while len(env.getErrors()) > 0:
-				#print('Bana nummer ' + str(i))
-				#print(state)
-				numIter = numIter + 1
-				observation = env.getObservation()
-				a, e = rl.choose_action(observation)
-				r = env.moveError(a, e)
-				new_observation = env.getObservation()
-				
-				rl.learn(observation[:,:,e], a, r, new_observation)
-				
-			print("Steps taken at iteration " +str(i) + ": ", numIter)
-			iterations[i] = numIter
-		print("Saving data in " + self.filename)
-		np.save(self.filename,iterations)
-		with open('trained_network.pkl', 'wb') as output:
-			rl.qnet.network.save('trainedNetwork.h5')
-		
+
+    def __init__(self):
+        #TODO värden som skall sättas innan en körning
+        self.maxNumberOfIterations = 10000
+
+        # creates a new filename each time we run the code
+        tmp = list('numSteps1.npy')
+        self.static_element = 1
+        while os.path.isfile("".join(tmp)):
+            self.static_element += 1
+            tmp[8] = str(self.static_element)
+        self.filename="".join(tmp)
+
+        self.run()
+        
+    def run(self):
+        flip = np.arange(5)
+        size=5
+        actions=4
+
+        rl = RLsys(4, size)
+
+        humRep=np.load('ToricCodeHuman.npy')
+        comRep=np.load('ToricCodeComputer.npy')
+        print(comRep[:,:,3])
+
+
+        iterations = np.zeros(comRep.shape[2])
+
+        for i in range(min(comRep.shape[2],self.maxNumberOfIterations)):
+            state=comRep[:,:,i]
+            env = Env(state)
+            numIter = 0
+
+            while len(env.getErrors()) > 0:
+                #print('Bana nummer ' + str(i))
+                #print(state)
+                numIter = numIter + 1
+                observation = env.getObservation()
+                a, e = rl.choose_action(observation)
+                r = env.moveError(a, e)
+                new_observation = env.getObservation()
+
+                rl.learn(observation[:,:,e], a, r, new_observation)
+
+            print("Steps taken at iteration " +str(i) + ": ", numIter)
+            iterations[i] = numIter
+
+        tmp = list('trainedNetwork1.h5')
+        tmp[14] = str(self.static_element)
+        filename = "".join(tmp)
+
+        print("Saving data in " + self.filename)
+        np.save(self.filename,iterations)
+
+        rl.qnet.network.save(filename)
+
         
        
 """""""""""""""""""""""""""""""""""""""""""""
 Mainmetod, här körs själva simuleringen.
 """""""""""""""""""""""""""""""""""""""""""""
 if __name__ == '__main__':
-	MainClass()
-	
-				
+    MainClass()
+
+
