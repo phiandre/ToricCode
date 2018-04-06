d2fd676c6bafdd8d2131935883fdd779d41ae1ac
guskarls <gustav.1996.k@hotmail.com>
2018-04-06 13:58:45 +0200

    Update PlotSteps.py

--- PlotSteps.py
+++ PlotSteps.py
@@ -9,5 +9,5 @@ plotData = np.zeros(round(len(data)/averageOver))
 for i in range(round(len(data)/averageOver)):
 	plotData[i] = np.average(data[i*averageOver:i*averageOver+(averageOver-1)])
 	
-plt.plot(np.arange(len(plotData)),plotData)
+plt.plot(np.arange(len(plotData)),plotData) # TODO: Fixa så grafen är över hela intervallet
 plt.show()
