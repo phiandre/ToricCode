03f9eeda39f701a7680deb6ffdc21f57a8d72e56
nihilsciens <hannouch@student.chalmers.se>
2018-04-09 11:12:03 +0200

    mindre fel fixat i rekursionen

--- runMC_ny.py
+++ runMC_ny.py
@@ -93,7 +93,7 @@ class MainClass:
 		action, error = self.rl.choose_action(observation)
 		# Hämta nuvarande och nästkommande reward
 		newReward = self.env.moveError(action, error)
-		upcomingReward = self.learnStep(newState, newReward)
+		upcomingReward = self.learnStep(copiedState, newReward)
 		# Uppdatera reward (som skall uppdateras i nätverket)
 		reward = newReward + self.gamma * upcomingReward
 		# Uppdatera nätverket
