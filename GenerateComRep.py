0b79856f130e40012946176c86a61ac8a67a45fd
Philip Andreasson <phiandr@student.chalmers.se>
2018-04-11 11:32:36 +0200

    la till lite kommentarer

--- GenerateComRep.py
+++ GenerateComRep.py
@@ -2,17 +2,22 @@ import numpy as np
 import random
 import pickle
 
-size=10
-numerror=4 #måste vara jämnt antal fel!
-iterations=10000
+
+size=10 #storlek på gitter
+numerror=4 #antal fel, måste vara jämnt antal fel!
+iterations=10000 #antal datapunkter som genereras
 computerRepresentation=np.zeros((size,size,iterations))
+
 for iter in range(iterations):
 	for i in range(numerror):
 		x=0
 		while x==0:
-			r=random.randint(0,(size-1))
-			c=random.randint(0,(size-1))
-			if computerRepresentation[r,c,iter]==0:
+			r=random.randint(0,(size-1)) #slumpa radindex
+			c=random.randint(0,(size-1)) #slumpa kolumnindex
+			
+			#Kolla så att ett fel inte redan finns i plaketten, annars
+			#slumpas indexen om. 
+			if computerRepresentation[r,c,iter]==0: 
 				computerRepresentation[r,c,iter]=1
 				x=1
 
--- PlotSteps.py
+++ PlotSteps.py
@@ -1,9 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+#ladda in datafilen 
 data = np.load('steps.npy')
 
+#ange över hur många värden datan medelvärdesbildas
 averageOver = 100
+
+
 plotData = np.zeros(round(len(data)/averageOver))
 
 for i in range(round(len(data)/averageOver)):
