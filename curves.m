e67008c6e4a30b055ca614a63afc270caaa39479
SimonDaNinja <37663175+SimonDaNinja@users.noreply.github.com>
2018-06-28 14:53:17 +0200

    matlabfilen är curves uppdaterad
    Man kan nu behålla konstant form på kurva oavsett antal iterationer om man vill.

--- curves.m
+++ curves.m
@@ -3,6 +3,12 @@ clc
 % Viktigt! För representativa kurvor, ange antal iterationer
 iterations = 200000;
 
+% För konstant form oavsett antal iterationer, sätt värde för kurvan till 1
+epsilonShape = 0;
+errorShape = 0;
+groundStateShape = 0;
+
+
 % 
 %       Epsilon Decay
 %
@@ -12,12 +18,15 @@ subplot(2,2,1)
 
 
 
-alfa = -0.8; % "flyttar änden" på kurvan
+alfa = -0.7; % "flyttar änden" på kurvan
 
 x = 1:iterations;
 
-k = 20000; % "flyttar mitten" på kurvan
-
+if epsilonShape ==1
+    k = iterations/10; % "flyttar mitten" på kurvan
+else
+    k = 20000;
+end
 y = ((k+x)/k).^alfa;
 
 
@@ -33,17 +42,28 @@ axis([1 iterations 0 1])
 %
 
 subplot(2,2,2)
+aa=1;
+bb=iterations;
 
+finalProbability = .1;
+initialProbability = .04;
 
+x=linspace(aa,bb);
 
-a = 1;
-b = iterations;
-x = a:b;
-
-pa = 0.02;
-pb = 0.10;
-
-p = (pb-pa)/(b-a)*x+pa;
+if errorShape ==1
+    w=pi/(0.125*iterations);
+    b=-0.13*iterations;
+else
+    w=pi/25000;
+    b=-26000;
+end
+    
+A=.5*finalProbability-.5*initialProbability;
+B=.5*finalProbability+.5*initialProbability;
+p=A*tanh(w*(x+b))+B;
+plot(x,p)
+grid on
+title('Ground State Reward')
 
 plot(x,p)
 grid on
@@ -60,8 +80,13 @@ bb=iterations;
 finalReward = 5;
 
 x=linspace(aa,bb);
-w=pi/90000;
-b=-78000;
+if groundStateShape ==1
+    w=pi/(0.275*iterations);
+    b=-0.39*iterations;
+else
+    w=pi/55000;
+    b=-78000;
+end
 A=.5*finalReward;
 B=.5*finalReward;
 y=A*tanh(w*(x+b))+B;
