8c6d5e13f2ce48af846d3c199a81809ae8b2c831
Philip Andreasson <phiandr@student.chalmers.se>
2018-08-02 15:08:29 +0200

    Spiel

---
+++ GUI.py
@@ -0,0 +1,100 @@
+import pygame
+import time
+import random
+
+pygame.init()
+
+display_width = 800
+display_height = 600
+
+
+gameDisplay = pygame.display.set_mode((display_width, display_height))
+pygame.display.set_caption("Game")
+clock = pygame.time.Clock()
+
+black = (0,0,0)
+white = (255,255,255)
+red = (255,0,0)
+
+car_width = 73
+
+
+carImg = pygame.image.load('racecar.png')
+
+
+def things(thingx, thingy, thingw, thingh, color):
+	pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh]) 
+
+def car(x,y):
+	gameDisplay.blit(carImg, (x,y))
+
+def text_objects(text, font):
+	textSurface = font.render(text, True, black)
+	return textSurface, textSurface.get_rect()
+
+def message_display(text):
+	largeText = pygame.font.Font('freesansbold.ttf',115)
+	TextSurf, TextRect = text_objects(text, largeText)
+	TextRect.center = ((display_width/2),(display_height/2))
+	gameDisplay.blit(TextSurf, TextRect)
+	pygame.display.update()
+	time.sleep(2)
+	
+
+def crash():
+	message_display("Pathetic")
+	game_loop()
+
+def game_loop():
+	x = display_width*0.45
+	y = display_height*0.8
+
+	x_change = 0
+	
+	thing_startx = random.randrange(0, display_width)
+	thing_starty = -600
+	thing_speed = random.randrange(8,23)
+	thing_width = 100
+	thing_height = 100
+	
+	gameExit = False
+
+	while not gameExit:
+		
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				quit()
+			
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_LEFT:
+					x_change = -15
+				if event.key == pygame.K_RIGHT:
+					x_change = 15
+				
+			if event.type == pygame.KEYUP:
+				if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+					x_change = 0
+			
+		x += x_change
+		gameDisplay.fill(white)
+		
+		things(thing_startx, thing_starty, thing_width, thing_height, black)
+		thing_starty += thing_speed
+		
+		car(x,y)
+		
+		if x > display_width - car_width or x < 0:
+			crash()
+			
+		
+		if thing_starty > display_height:
+			thing_starty = 0 - thing_height
+			thing_startx = random.randrange(0,display_width)
+		
+		pygame.display.update()
+		clock.tick(60)
+
+game_loop()
+pygame.quit()
+quit()
