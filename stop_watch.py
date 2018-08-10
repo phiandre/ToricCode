65be9d6c01e2380c0481b1f18481b121040b25ff
SimonDaNinja <37663175+SimonDaNinja@users.noreply.github.com>
2018-08-10 09:22:06 +0200

    StopWatch har nya funktioner
    Allt som har med intervals att göra är nu mer användarvänligt.

--- stop_watch.py
+++ stop_watch.py
@@ -6,26 +6,50 @@ class StopWatch:
 		self.Stop = time.time()
 		self.IntervalStart = time.time()
 		self.IntervalStop = time.time()
-		self.Intervals =  []
+		self.Intervals =  [0]
 		self.n = 0
+		self.passedTime = 0
+		self.intervalTime = 0
+		self.currentInterval = 0
+		
 	def start(self):
 		self.Start = time.time()
 		self.Stop = time.time()
 		self.IntervalStart = time.time()
 		self.IntervalStop = time.time()
-		self.Intervals =  []
+		self.Intervals =  [0]
 		self.n = 0
+		self.passedTime = 0
+		self.intervalTime = 0
+		self.currentInterval = 0
+		
 	def stop(self):
 		self.Stop = time.time()
-		return self.Stop-self.Start
+		self.passedTime = self.Stop-self.Start
+		self.intervalTime = self.Stop - self.IntervalStart
+		self.Intervals[self.currentInterval] = self.intervalTime
+		return self.passedTime
+		
 	def interval(self):
 		self.IntervalStop = time.time()
 		intervalTime = self.IntervalStop - self.IntervalStart
 		self.IntervalStart = time.time()
-		self.Intervals.append(intervalTime)
+		self.Intervals[self.currentInterval] = intervalTime
+		self.Intervals.append(0)
+		self.currentInterval +=1
 		self.n += 1
 		return intervalTime
+		
 	def average(self):
 		return sum(self.Intervals)/self.n
+	
+	def pause(self):
+		self.passedTime = time.time() - self.Start
+		self.intervalTime = time.time()- self.IntervalStart
+		return self.passedTime
+	
+	def unpause(self):
+		self.Start = time.time() - self.passedTime
+		self.IntervalStart = time.time() - self.intervalTime
 	def totalTime(self):
 		return sum(self.Intervals)
